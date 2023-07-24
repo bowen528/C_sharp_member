2fc00b3822ce9746c4598af8251539385666a232
bowen528 <hhilary5432@gmail.com>
2023-07-25 02:18:38 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace member
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_FullName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdbtn_GdM = new System.Windows.Forms.RadioButton();
+            this.rdbtn_GdF = new System.Windows.Forms.RadioButton();
+            this.rdbtn_BTA = new System.Windows.Forms.RadioButton();
+            this.rdbtn_BTB = new System.Windows.Forms.RadioButton();
+            this.rdbtn_BTO = new System.Windows.Forms.RadioButton();
+            this.rdbtn_BTAB = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Prv = new System.Windows.Forms.Button();
+            this.lab_Opt = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbx_BDY = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbx_BDM = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbx_BDD = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(28, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名：";
+            // 
+            // tbx_FullName
+            // 
+            this.tbx_FullName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbx_FullName.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbx_FullName.Location = new System.Drawing.Point(101, 7);
+            this.tbx_FullName.Name = "tbx_FullName";
+            this.tbx_FullName.Size = new System.Drawing.Size(146, 30);
+            this.tbx_FullName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(28, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "性別：";
+            // 
+            // rdbtn_GdM
+            // 
+            this.rdbtn_GdM.AutoSize = true;
+            this.rdbtn_GdM.Checked = true;
+            this.rdbtn_GdM.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdbtn_GdM.Location = new System.Drawing.Point(101, 50);
+            this.rdbtn_GdM.Name = "rdbtn_GdM";
+            this.rdbtn_GdM.Size = new System.Drawing.Size(58, 20);
+            this.rdbtn_GdM.TabIndex = 3;
+            this.rdbtn_GdM.TabStop = true;
+            this.rdbtn_GdM.Text = "男性";
+            this.rdbtn_GdM.UseVisualStyleBackColor = true;
+            // 
+            // rdbtn_GdF
+            // 
+            this.rdbtn_GdF.AutoSize = true;
+            this.rdbtn_GdF.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdbtn_GdF.Location = new System.Drawing.Point(189, 50);
+            this.rdbtn_GdF.Name = "rdbtn_GdF";
+            this.rdbtn_GdF.Size = new System.Drawing.Size(58, 20);
+            this.rdbtn_GdF.TabIndex = 3;
+            this.rdbtn_GdF.Text = "女性";
+            this.rdbtn_GdF.UseVisualStyleBackColor = true;
+            // 
+            // rdbtn_BTA
+            // 
+            this.rdbtn_BTA.AutoSize = true;
+            this.rdbtn_BTA.Checked = true;
+            this.rdbtn_BTA.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdbtn_BTA.Location = new System.Drawing.Point(83, 37);
+            this.rdbtn_BTA.Name = "rdbtn_BTA";
+            this.rdbtn_BTA.Size = new System.Drawing.Size(53, 20);
+            this.rdbtn_BTA.TabIndex = 5;
+            this.rdbtn_BTA.TabStop = true;
+            this.rdbtn_BTA.Text = "A型";
+            this.rdbtn_BTA.UseVisualStyleBackColor = true;
+            // 
+            // rdbtn_BTB
+            // 
+            this.rdbtn_BTB.AutoSize = true;
+            this.rdbtn_BTB.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdbtn_BTB.Location = new System.Drawing.Point(171, 37);
+            this.rdbtn_BTB.Name = "rdbtn_BTB";
+            this.rdbtn_BTB.Size = new System.Drawing.Size(52, 20);
+            this.rdbtn_BTB.TabIndex = 5;
+            this.rdbtn_BTB.Text = "B型";
+            this.rdbtn_BTB.UseVisualStyleBackColor = true;
+            // 
+            // rdbtn_BTO
+            // 
+            this.rdbtn_BTO.AutoSize = true;
+            this.rdbtn_BTO.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdbtn_BTO.Location = new System.Drawing.Point(83, 84);
+            this.rdbtn_BTO.Name = "rdbtn_BTO";
+            this.rdbtn_BTO.Size = new System.Drawing.Size(53, 20);
+            this.rdbtn_BTO.TabIndex = 5;
+            this.rdbtn_BTO.Text = "O型";
+            this.rdbtn_BTO.UseVisualStyleBackColor = true;
+            // 
+            // rdbtn_BTAB
+            // 
+            this.rdbtn_BTAB.AutoSize = true;
+            this.rdbtn_BTAB.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdbtn_BTAB.Location = new System.Drawing.Point(171, 84);
+            this.rdbtn_BTAB.Name = "rdbtn_BTAB";
+            this.rdbtn_BTAB.Size = new System.Drawing.Size(63, 20);
+            this.rdbtn_BTAB.TabIndex = 5;
+            this.rdbtn_BTAB.Text = "AB型";
+            this.rdbtn_BTAB.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbtn_BTA);
+            this.groupBox1.Controls.Add(this.rdbtn_BTAB);
+            this.groupBox1.Controls.Add(this.rdbtn_BTO);
+            this.groupBox1.Controls.Add(this.rdbtn_BTB);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(21, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 137);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "血型：";
+            // 
+            // btn_Prv
+            // 
+            this.btn_Prv.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_Prv.Location = new System.Drawing.Point(32, 329);
+            this.btn_Prv.Name = "btn_Prv";
+            this.btn_Prv.Size = new System.Drawing.Size(253, 43);
+            this.btn_Prv.TabIndex = 7;
+            this.btn_Prv.Text = "顯示個人基本資料";
+            this.btn_Prv.UseVisualStyleBackColor = true;
+            this.btn_Prv.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn_Prv_MouseClick);
+            // 
+            // lab_Opt
+            // 
+            this.lab_Opt.BackColor = System.Drawing.Color.White;
+            this.lab_Opt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lab_Opt.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lab_Opt.Location = new System.Drawing.Point(32, 375);
+            this.lab_Opt.Name = "lab_Opt";
+            this.lab_Opt.Size = new System.Drawing.Size(253, 157);
+            this.lab_Opt.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(28, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "生日：";
+            // 
+            // cbx_BDY
+            // 
+            this.cbx_BDY.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbx_BDY.FormattingEnabled = true;
+            this.cbx_BDY.Location = new System.Drawing.Point(32, 271);
+            this.cbx_BDY.Name = "cbx_BDY";
+            this.cbx_BDY.Size = new System.Drawing.Size(63, 24);
+            this.cbx_BDY.TabIndex = 9;
+            this.cbx_BDY.Text = "2000";
+            this.cbx_BDY.SelectedIndexChanged += new System.EventHandler(this.cbx_BDY_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(97, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "年";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(173, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 24);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "月";
+            // 
+            // cbx_BDM
+            // 
+            this.cbx_BDM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_BDM.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbx_BDM.FormattingEnabled = true;
+            this.cbx_BDM.Location = new System.Drawing.Point(128, 271);
+            this.cbx_BDM.Name = "cbx_BDM";
+            this.cbx_BDM.Size = new System.Drawing.Size(43, 24);
+            this.cbx_BDM.TabIndex = 12;
+            this.cbx_BDM.SelectedIndexChanged += new System.EventHandler(this.cbx_BDM_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(249, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "日";
+            // 
+            // cbx_BDD
+            // 
+            this.cbx_BDD.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_BDD.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbx_BDD.FormattingEnabled = true;
+            this.cbx_BDD.Location = new System.Drawing.Point(204, 271);
+            this.cbx_BDD.Name = "cbx_BDD";
+            this.cbx_BDD.Size = new System.Drawing.Size(43, 24);
+            this.cbx_BDD.TabIndex = 31;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 568);
+            this.ControlBox = false;
+            this.Controls.Add(this.cbx_BDD);
+            this.Controls.Add(this.cbx_BDM);
+            this.Controls.Add(this.cbx_BDY);
+            this.Controls.Add(this.lab_Opt);
+            this.Controls.Add(this.btn_Prv);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rdbtn_GdF);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rdbtn_GdM);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx_FullName);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "基本資料註冊";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_FullName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rdbtn_GdM;
+        private System.Windows.Forms.RadioButton rdbtn_GdF;
+        private System.Windows.Forms.RadioButton rdbtn_BTA;
+        private System.Windows.Forms.RadioButton rdbtn_BTB;
+        private System.Windows.Forms.RadioButton rdbtn_BTO;
+        private System.Windows.Forms.RadioButton rdbtn_BTAB;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Prv;
+        private System.Windows.Forms.Label lab_Opt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbx_BDY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbx_BDM;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbx_BDD;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace member
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Prv_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Prv_MouseClick(object sender, MouseEventArgs e)
+        {
+            string sOpt = "";
+
+            if (tbx_FullName.Text != "")
+            { sOpt += "姓名： " + tbx_FullName.Text+"\n";}
+
+            if (rdbtn_GdF.Checked == true)
+            { sOpt += "性別：" + rdbtn_GdF.Text + "\n"; }
+            else
+            { sOpt += "性別：" + rdbtn_GdM.Text + "\n"; }
+
+
+            if (rdbtn_BTA.Checked == true)
+            { sOpt += "血型：" + rdbtn_BTA.Text + "\n"; }
+            else if (rdbtn_BTB.Checked == true)
+            { sOpt += "血型：" + rdbtn_BTB.Text + "\n"; }
+            else if (rdbtn_BTO.Checked == true)
+            { sOpt += "血型：" + rdbtn_BTO.Text + "\n"; }
+            else
+            { sOpt += "血型：" + rdbtn_BTAB.Text + "\n"; }
+
+
+            if(cbx_BDY.Text != "" && cbx_BDM.Text != "" && cbx_BDD.Text != "")
+            { sOpt += "生日： " + cbx_BDY.Text + " 年 " + cbx_BDM.Text + " 月 " + cbx_BDD.Text + " 日 "; }
+
+            lab_Opt.Text = sOpt;
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //下方產生月份的下拉選單 清單項目內容
+            for (int i = 1 ; i <= 12 ; i++) 
+            { cbx_BDM.Items.Add(i); }
+           
+            //下方指定 月份下拉選單自動取得系統月份值，做為選單的預設值
+            cbx_BDM.Text = DateTime.Today.Month.ToString();
+
+            //下方產生年份的下拉選單 清單項目(今天的年-> 130年前的年)內容
+            int iYoT = DateTime.Today.Year;
+            for (int i = iYoT; i >= iYoT - 130; i--) 
+            { cbx_BDY.Items.Add(i); }
+
+            //下方產生日的下拉選單 清單項目
+            vSetDay();
+        }
+
+        private void cbx_BDY_SelectedIndexChanged(object sender, EventArgs e)
+        { vSetDay(); }
+
+        private void cbx_BDM_SelectedIndexChanged(object sender, EventArgs e)
+        { vSetDay(); }
+
+        private void vSetDay() 
+        {
+            //下方產生日的下拉選單 清單項目
+            cbx_BDD.Items.Clear();
+            int iMth = int.Parse(cbx_BDM.Text); //取得使用者目前取得的月份值
+
+            int iDays = 0; //取得使用者選擇月份值的天數
+            if (iMth == 2)
+            {
+                int iYr = int.Parse(cbx_BDY.Text);
+                if (iYr % 400 == 0)
+                { iDays = 29; }
+                else
+                {
+                    if (iYr % 4 == 0 && iYr % 100 != 0)
+                    { iDays = 29; }
+                    else
+                    { iDays = 28; }
+                }
+
+            }
+            else if (iMth == 4 || iMth == 6 || iMth == 9 || iMth == 11)
+            { iDays = 30; }
+            else
+            { iDays = 31; }
+
+
+            for (int i = 1; i <= iDays; i++)
+            { cbx_BDD.Items.Add(i); }
+        }
+    }
+}
